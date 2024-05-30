06107c6931c19028d0b579b4d8a9fdd6b7e80136
redCodev <redcodev@icloud.com>
2024-05-29 22:30:31 -0300

    filtro para listar apenas itens vendidos

--- LeiloesTDSat/src/listagemVIEW.java
+++ LeiloesTDSat/src/listagemVIEW.java
@@ -158,8 +158,8 @@ public class listagemVIEW extends javax.swing.JFrame {
     }//GEN-LAST:event_btnVenderActionPerformed
 
     private void btnVendasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVendasActionPerformed
-        //vendasView vendas = new vendasView(); 
-        //vendas.setVisible(true);
+        vendasView vendas = new vendasView(); 
+        vendas.setVisible(true);
     }//GEN-LAST:event_btnVendasActionPerformed
 
     private void btnVoltarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVoltarActionPerformed
